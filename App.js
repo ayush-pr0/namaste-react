53b2d917920ef133f3d9ec456b19888ea16b336f
ayush-pr0 <ayushkhandelwal9083@gmail.com>
2023-07-23 16:52:11 +0530

    initial commit (day 1)

---
+++ App.js
@@ -0,0 +1,15 @@
+const div = React.createElement('div', {id: "parent"}, 
+                [
+                    React.createElement('div', {id: "child-1"}, 
+                    [
+                        React.createElement('h1', {}, "<h1> tag"), React.createElement('h2', {}, "<h2> tag")
+                    ]),
+                    React.createElement('div', {id: "child-2"}, 
+                    [
+                        React.createElement('h1', {}, "<h1> tag"), React.createElement('h2', {}, "<h2> tag")
+                    ])
+                ]
+            );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
