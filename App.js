a4c4ece412ac3b9faae758f21e1e023efd94caf0
ayush-pr0 <ayushkhandelwal9083@gmail.com>
2023-08-01 12:43:48 +0530

    day 3 done

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const div = React.createElement('div', {id: "parent"}, 
-                [
-                    React.createElement('div', {id: "child-1"}, 
-                    [
-                        React.createElement('h1', {}, "<h1> tag"), React.createElement('h2', {}, "<h2> tag")
-                    ]),
-                    React.createElement('div', {id: "child-2"}, 
-                    [
-                        React.createElement('h1', {}, "<h1> tag"), React.createElement('h2', {}, "<h2> tag")
-                    ])
-                ]
-            );
+// const div = React.createElement('div', {id: "parent"},
+//                 [
+//                     React.createElement('div', {id: "child-1"},
+//                     [
+//                         React.createElement('h1', {}, "<h1> tag"), React.createElement('h2', {}, "<h2> tag")
+//                     ]),
+//                     React.createElement('div', {id: "child-2"},
+//                     [
+//                         React.createElement('h1', {}, "<h1> tag"), React.createElement('h2', {}, "<h2> tag")
+//                     ])
+//                 ]
+//             );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(div);
+// JSX => React.createElement => ReactElement (JS Object) => HTMLElement (when Rendered)
+const Title = () => {
+  return <h1>My Introduction</h1>;
+};
+
+const DivElement = () => {
+  return (
+    <div className="container">
+      <Title />
+      <p>My name is Ayush Khandelwal, B.tech Graduate in 2023.</p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<DivElement />);
